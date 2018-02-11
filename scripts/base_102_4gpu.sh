64a39c80c1f5dbd8f9fd1f20f1d14160c1b36665
hli2020 <hongyangli2020@gmail.com>
2018-02-11 20:08:05 +0800

    tiny update

--- scripts/base_102_4gpu.sh
+++ scripts/base_102_4gpu.sh
@@ -3,4 +3,10 @@
 python2 tools/train_net.py \
     --multi-gpu-testing \
     --cfg configs/12_2017_baselines/retinanet_X-101-64x4d-FPN_2x.yaml \
-    OUTPUT_DIR results/detectron-output-4gpu_retina
+    OUTPUT_DIR results/detectron-output-4gpu_retina \
+    NUM_GPUS 4 \
+    TRAIN.SCALES 550, \
+    TRAIN.MAX_SIZE 900 \
+    TRAIN.IMS_PER_BATCH 1 \
+    TEST.SCALES 550, \
+    TEST.MAX_SIZE 900
